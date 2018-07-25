fe5132e2ed926bbc42ae14c7a3d37679f0cfc339
Suhas Chintala <suchin@microsoft.com>
2018-07-25 20:33:48 +0530

    GG

--- myapp/views.py
+++ myapp/views.py
@@ -3,9 +3,9 @@ from django.http import HttpResponse
 # Create your views here.
 def myview(request):
     from bs4 import BeautifulSoup
-    # import requests
-    # req = requests.get("https://www.msn.com/en-in/entertainment/bollywood/yay-he-called-me-a-film-star-abhishek-bachchans-reaction-to-trolls-is-sarcasm-goals-no/ar-BBL1peZ?ocid=spartandhp")
-    text = req.body
+    import requests
+    req = requests.get("https://www.msn.com/en-in/entertainment/bollywood/yay-he-called-me-a-film-star-abhishek-bachchans-reaction-to-trolls-is-sarcasm-goals-no/ar-BBL1peZ?ocid=spartandhp")
+    text = req.text
     soup = BeautifulSoup(text)
     finalstring = ""
     for data in soup.find_all('p'):
